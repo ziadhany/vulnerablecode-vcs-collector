6d70cb29fc66f23dabe1edc13af92a103828740f
ziad hany <ziadhany2016@gmail.com>
2026-03-29 05:06:45 +0200

    Add a checkpoints for pocs collector
    Signed-off-by: ziad hany <ziadhany2016@gmail.com>

--- pocs_collector.py
+++ pocs_collector.py
@@ -18,12 +18,22 @@ from fetchcode.vcs import fetch_via_vcs
 
 load_dotenv()
 
+BATCH_SIZE = 1000
+
 class PocsCollector(BasePipeline):
     @classmethod
     def steps(cls):
         return (
             cls.collect_items,
         )
+
+    def get_start_index(self):
+        with open("config/checkpoints.json", "r") as f:
+            return json.load(f).get("index", 0)
+
+    def save_index(self, index):
+        with open("config/checkpoints.json", "w") as f:
+            json.dump({"index": index}, f)
 
     def log(self, message):
         now_local = datetime.now(timezone.utc).astimezone()
@@ -33,9 +43,11 @@ class PocsCollector(BasePipeline):
 
     def get_cve_list(self):
         vcs_response = fetch_via_vcs("git+https://github.com/CVEProject/cvelistV5")
+        cves = set()
         for file_path in Path(vcs_response.dest_dir).glob("**/*.json"):
             if file_path.is_file() and file_path.name.startswith("CVE-"):
-                yield file_path.stem
+                cves.add(file_path.stem)
+        return sorted(cves)
 
     def get_pocs_repo_urls(self, cve_id):
         """Searches GitHub and returns a list of POCS URLs"""
@@ -57,7 +69,11 @@ class PocsCollector(BasePipeline):
         return sorted(list(pocs_urls))
 
     def collect_items(self):
-        for cve_id in self.get_cve_list():
+        total_cves = self.get_cve_list()
+        start_index = self.get_start_index()
+        end_index = start_index + BATCH_SIZE
+        self.log(f"Batch Started from index {start_index} to {end_index}")
+        for cve_id in total_cves[start_index:end_index]:
             pocs_repo_urls = self.get_pocs_repo_urls(cve_id)
             if not pocs_repo_urls:
                 continue
@@ -73,6 +89,10 @@ class PocsCollector(BasePipeline):
 
             with open(path, "w", encoding="utf-8") as f:
                 json.dump(data, f, indent=2)
+
+        next_index = end_index if end_index < len(total_cves) else 0
+        self.save_index(next_index)
+        self.log(f"Batch complete. Saved next index: {next_index}")
 
 if __name__ == "__main__":
     github_token = os.getenv("GH_API_TOKEN")
